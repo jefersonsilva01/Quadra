1de14dc722938d2611a5fd2cf31c4f969cbb86e7
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-06-06 11:49:24 -0300

    Add car info

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`)
