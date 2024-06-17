0e5e194aaab084291c5e6b6cda3fc9ea0692aa6d
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-06-17 17:39:06 -0300

    Add header mobile responsive

--- public/js/home.js
+++ public/js/home.js
@@ -57,19 +57,25 @@ window.addEventListener("scroll", showSpecifications);
 
 // Nav color function
 
-const navBar = document.querySelector('.navbar')
-const navTitle = document.querySelector('.nav-title')
-const navLink = document.querySelectorAll('.nav-link')
+const navBar = document.querySelector('.navbar');
+const navTitle = document.querySelector('.nav-title');
+const navLink = document.querySelectorAll('.nav-link');
+const navbarToggler = document.querySelector('.navbar-toggler svg');
 
 const navColorChange = () => {
   if (window.scrollY >= 120) {
-    navBar.classList.add('nav-scroll')
-    navTitle.classList.add('nav-title-scroll')
+    navBar.classList.add('nav-scroll');
+    navTitle.classList.add('nav-title-scroll');
     navLink.forEach(element => element.classList.add('nav-link-scroll'));
+    navbarToggler.removeAttribute("fill");
+    navbarToggler.setAttribute("fill", "rgb(24, 24, 26)");
   } else {
-    navBar.classList.remove('nav-scroll')
-    navTitle.classList.remove('nav-title-scroll')
+    navBar.classList.remove('nav-scroll');
+    navTitle.classList.remove('nav-title-scroll');
     navLink.forEach(element => element.classList.remove('nav-link-scroll'));
+    navbarToggler.removeAttribute("fill");
+    navbarToggler.setAttribute("fill", "rgb(255, 255, 255)");
+
   }
 }
 
