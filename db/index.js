772df43ea9b4ee4a41d97441d9cefdf18d804020
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-06-18 18:36:27 -0300

    Update mongo connect

--- db/index.js
+++ db/index.js
@@ -1,16 +1,39 @@
-const mongoose = require("mongoose");
-
 // Mongo connection development
-// const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/quadra";
+/* const mongoose = require("mongoose");
 
-//Mongo connection production
-const MONGO_URI = process.env.MONGO_URI_DEPLOY;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/quadra";
 
 mongoose.connect(MONGO_URI)
-  .then(x => {
-    const databaseName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+.then(x => {
+  const databaseName = x.connections[0].name;
+  console.log(`Connected to Mongo! Database name: "${databaseName}"`);
   })
   .catch(err => {
     console.log("Error connecting to mongo: ", err);
-  });
+    }); */
+
+//Mongo connection production
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const MONGO_URI = process.env.MONGO_URI_DEPLOY;
+
+const client = new MongoClient(MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
